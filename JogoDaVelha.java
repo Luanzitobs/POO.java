67eab8189d649cf78b6f7a960cc6f505aea9384d
Luanzitobs <luan.bentto2017@gmail.com>
2026-04-14 10:23:17 -0300

    JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -12,52 +12,63 @@ public class JogoDaVelha {
         boolean jogoAcabou = false;
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("--- BEM-VINDO AO JOGO DA VELHA ---");
+
         while (!jogoAcabou) {
             exibirTabuleiro(tabuleiro);
-            System.out.println("Jogador " + jogadorAtual + ", digite linha e coluna (0-2): ");
+            System.out.println("\nJogador " + jogadorAtual + ", sua vez!");
+            System.out.print("Digite a LINHA (0-2) e a COLUNA (0-2) separadas por espaço: ");
 
-            int linha = scanner.nextInt();
-            int col = scanner.nextInt();
+            
+            if (scanner.hasNextInt()) {
+                int linha = scanner.nextInt();
+                int col = scanner.nextInt();
 
-            if (linha >= 0 && linha < 3 && col >= 0 && col < 3 && tabuleiro[linha][col] == ' ') {
-                tabuleiro[linha][col] = jogadorAtual;
-                jogoAcabou = verificarVitoria(tabuleiro, jogadorAtual);
+                if (linha >= 0 && linha < 3 && col >= 0 && col < 3 && tabuleiro[linha][col] == ' ') {
+                    tabuleiro[linha][col] = jogadorAtual;
+                    jogoAcabou = verificarVitoria(tabuleiro, jogadorAtual);
 
-                if (jogoAcabou) {
-                    exibirTabuleiro(tabuleiro);
-                    System.out.println("Parabéns! Jogador " + jogadorAtual + " venceu!");
-                } else if (tabuleiroCheio(tabuleiro)) {
-                    exibirTabuleiro(tabuleiro);
-                    System.out.println("Empate! Deu velha.");
-                    jogoAcabou = true;
+                    if (jogoAcabou) {
+                        exibirTabuleiro(tabuleiro);
+                        System.out.println("\n⭐ PARABÉNS! O Jogador " + jogadorAtual + " venceu! ⭐");
+                    } else if (tabuleiroCheio(tabuleiro)) {
+                        exibirTabuleiro(tabuleiro);
+                        System.out.println("\n🤝 EMPATE! Deu velha.");
+                        jogoAcabou = true;
+                    } else {
+                        jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+                    }
                 } else {
-                    jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+                    System.out.println("\n[!] MOVIMENTO INVÁLIDO: A posição ["+linha+","+col+"] está ocupada ou fora do limite (0-2).");
                 }
             } else {
-                System.out.println("Movimento inválido. Tente novamente.");
+                System.out.println("\n[!] ERRO: Digite apenas números inteiros.");
+                scanner.next(); 
             }
         }
         scanner.close();
     }
 
     public static void exibirTabuleiro(char[][] tabuleiro) {
-        System.out.println("-------------");
+        System.out.println("\n    COLS");
+        System.out.println("    0   1   2"); 
+        System.out.println("  -------------");
         for (int i = 0; i < 3; i++) {
-            System.out.print("| ");
+            System.out.print(i + " | "); 
             for (int j = 0; j < 3; j++) {
                 System.out.print(tabuleiro[i][j] + " | ");
             }
-            System.out.println("\n-------------");
+            System.out.println("\n  -------------");
         }
     }
 
     public static boolean verificarVitoria(char[][] t, char j) {
-        // Linhas e Colunas
         for (int i = 0; i < 3; i++) {
+            
             if ((t[i][0] == j && t[i][1] == j && t[i][2] == j) ||
                     (t[0][i] == j && t[1][i] == j && t[2][i] == j)) return true;
         }
-        // Diagonais
+        
         return (t[0][0] == j && t[1][1] == j && t[2][2] == j) ||
                 (t[0][2] == j && t[1][1] == j && t[2][0] == j);
     }
